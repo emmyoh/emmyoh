ef8b7e2c61f3886a285e2780eb48ecff1605cb76
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2026-04-26 15:10:38 -0700

    Update update-mac.sh

--- update-mac.sh
+++ update-mac.sh
@@ -1,1 +1,10 @@
-brew update; brew upgrade; rustup update; conda update --all
+#!/bin/bash
+trap "exit" INT TERM; trap "kill 0" EXIT; sudo -v || exit $?; sleep 1; while true; do sleep 60; sudo -nv; done 2>/dev/null &
+brew update
+brew upgrade
+brew upgrade --cask
+sudo port clean --all all
+sudo port selfupdate
+sudo port upgrade outdated
+rustup update
+#conda update --all --yes
